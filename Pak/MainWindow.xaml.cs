ccb910072f299759185691e82487b48c3a47f021
Clint Pearson <clint.pearson@kpmg.co.uk>
2015-06-19 19:06:44 +0100

    Initial Commit with image production.

---
+++ Pak/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using MahApps.Metro.Controls;
+
+namespace Pak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Image.Events().MouseLeftButtonDown.Subscribe(_ =>
+            {
+                DragDrop.DoDragDrop(Image, new DataObject(DataFormats.FileDrop, new[] {(string) Image.Tag}),
+                    DragDropEffects.Copy);
+                
+            });
+
+            Message.Text = DropMessage;
+
+            DragDrop.AddDropHandler(Border, OnDrop);
+        }
+
+        
+
+        private const string DropMessage = "Drop Files or Pak'd Image Here";
+
+        private async void OnDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data;
+
+            if (data.GetDataPresent(DataFormats.Text))
+            {
+                var uri = new Uri((string) data.GetData(DataFormats.Text));
+                Bitmap bmp;
+                using (var client = new WebClient())
+                {
+                    var imgData = await client.DownloadDataTaskAsync(uri);
+                    bmp = new Bitmap(new MemoryStream(imgData));
+                }
+            }else if (data.GetDataPresent(DataFormats.FileDrop))
+            {
+                DropPanel.Visibility = Visibility.Collapsed;
+                ProgessPanel.Visibility = Visibility.Visible;
+                DragDrop.RemoveDropHandler(Border, OnDrop);
+                var dat = (string[])data.GetData(DataFormats.FileDrop);
+                if (dat.Length > 1)
+                {
+                    MessageBox.Show(this, "If you want to share multiple files at the same time, please zip / 7-zip them first.", "Multiple Files",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning);
+                    return;
+                }
+
+                var imgPath = Path.GetTempFileName();
+                var newName = Path.Combine(Path.GetDirectoryName(imgPath), Path.GetFileNameWithoutExtension(imgPath) + ".png");
+                File.Move(imgPath, newName);
+                imgPath = newName;
+                await Task.Run(() => Packer.CreateImage(dat[0], imgPath));
+                var bi = new BitmapImage();
+                bi.BeginInit();
+                bi.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                bi.CacheOption = BitmapCacheOption.None;
+                bi.UriSource = new Uri(imgPath, UriKind.Absolute);
+                bi.EndInit();
+                Image.Source = bi;
+                Image.Tag = imgPath;
+                Message.Text = "Drag Image to Comment Box";
+                GoAgainButton.Visibility = Visibility.Visible;
+                ProgessPanel.Visibility = Visibility.Collapsed;
+                DropPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private void GoAgainButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            GoAgainButton.Visibility = Visibility.Collapsed;
+            Image.Source = null;
+            Message.Text = DropMessage;
+            DragDrop.AddDropHandler(Border, OnDrop);
+        }
+    }
+}
---
+++ Pak/Packer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SevenZip;
+
+namespace Pak
+{
+    public static class Packer
+    {
+        private static byte[] CompressFile(string filePath)
+        {
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    SevenZipCompressor.CompressStream(fs, ms, null, null);
+                    return ms.GetBuffer();
+                }
+            }
+        }
+
+        private static Tuple<int, byte[]> DropTrailingZeroes(byte[] bytes)
+        {
+            var count = 0;
+            var length = bytes.Length;
+            for (var i = length - 1; i > 0; i--)
+            {
+                if (bytes[i] == 0)
+                {
+                    count++;
+                    continue;
+                }
+                break;
+            }
+            return Tuple.Create(count, bytes.Take(length - count).ToArray());
+        }
+
+        public static void CreateImage(string sourceFile, string targetPath)
+        {
+            var dropped = DropTrailingZeroes(CompressFile(sourceFile));
+            var fNameBytes = Encoding.UTF8.GetBytes(Path.GetFileName(sourceFile));
+            var fNameLengthBytes = BitConverter.GetBytes(fNameBytes.Length);
+            var lengthBytes = BitConverter.GetBytes(dropped.Item2.Length);
+            var droppedBytes = BitConverter.GetBytes(dropped.Item1);
+
+            var data = new Queue<byte[]>(
+                fNameLengthBytes.Concat(fNameBytes).Concat(droppedBytes).Concat(droppedBytes).Concat(lengthBytes).Concat(dropped.Item2)
+                    .Select((item, idx) => new {item, idx})
+                    .GroupBy(x => x.idx/4)
+                    .Select(g => g.Select(x => x.item).ToArray()));
+
+            var cols = (int) Math.Sqrt(data.Count);
+            var rows = (int) Math.Ceiling((double) data.Count/cols);
+
+            using (var bmp = new Bitmap(cols, rows))
+            {
+                var rect = new Rectangle(0, 0, cols, rows);
+                var locked = bmp.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+                var stride = locked.Stride;
+                unsafe
+                {
+                    var ptr = (byte*) locked.Scan0;
+                    for (var y = 0; y < rows; y++)
+                    {
+                        for (var x = 0; x < cols; x ++)
+                        {
+                            if (data.Count == 0) break;
+                            var grp = data.Dequeue();
+
+                            ptr[(y*stride) + (x*4)] = grp.ElementAtOrDefault(0);
+                            ptr[(y*stride) + (x*4) + 1] = grp.ElementAtOrDefault(1);
+                            ptr[(y*stride) + (x*4) + 2] = grp.ElementAtOrDefault(2);
+                            ptr[(y*stride) + (x*4) + 3] = grp.ElementAtOrDefault(3);
+                        }
+                    }
+                }
+                bmp.UnlockBits(locked);
+                bmp.Save(targetPath, ImageFormat.Png);
+            }
+        }
+    }
+}
